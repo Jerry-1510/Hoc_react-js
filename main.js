f6d6ca14c116a8518d70dfccbc4a3d3503779936
TUAN <btuancreator@gmail.com>
2025-05-21 08:56:29 +0700

    cập nhật lần 1

---
+++ main.js
@@ -0,0 +1,10 @@
+// import { myVar, test } from "./named.js";
+// console.log(myVar);
+// console.log(test());
+
+// lấy tất cả
+// import * as name from "./named.js";
+// console.log(name.myVar);
+
+import total from "./named.js";
+console.log(total(4, 5));
